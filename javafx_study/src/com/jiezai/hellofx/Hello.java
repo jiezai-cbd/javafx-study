3edf2adc386655f11765779f9229343468abbef5
jiezai-cbd <wwjpcx@163.com>
2020-06-08 21:36:30 +0800

    hello javafx

---
+++ javafx_study/src/com/jiezai/hellofx/Hello.java
@@ -0,0 +1,17 @@
+package com.jiezai.hellofx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Hello extends Application {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
