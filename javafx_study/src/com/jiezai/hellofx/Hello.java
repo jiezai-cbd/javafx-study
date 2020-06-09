55e17c996e381b31f829439ee0bf66eb697a4c91
jiezai-cbd <wwjpcx@163.com>
2020-06-09 10:59:06 +0800

    StageStyle

--- javafx_study/src/com/jiezai/hellofx/Hello.java
+++ javafx_study/src/com/jiezai/hellofx/Hello.java
@@ -8,6 +8,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Hello extends Application {
     public static void main(String[] args) {
@@ -48,26 +49,50 @@ public class Hello extends Application {
 //                System.out.println("width:"+newValue.doubleValue());
 //            }
 //        });
-        primaryStage.xProperty().addListener(new ChangeListener<Number>(){
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("x:"+newValue);
-            }
-        });
-        primaryStage.yProperty().addListener(new ChangeListener<Number>(){
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("y:"+newValue);
-            }
-        });
+//        primaryStage.xProperty().addListener(new ChangeListener<Number>(){
+//            @Override
+//            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+//                System.out.println("x:"+newValue);
+//            }
+//        });
+//        primaryStage.yProperty().addListener(new ChangeListener<Number>(){
+//            @Override
+//            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+//                System.out.println("y:"+newValue);
+//            }
+//        });
 //        primaryStage.setFullScreen(true);
 //
 //        primaryStage.setScene(new Scene(new Group()));
 //        primaryStage.setOpacity(0.5);
-        primaryStage.setAlwaysOnTop(true);
+        //primaryStage.setAlwaysOnTop(true);
+//        Stage s1 = new Stage();
+//        s1.setTitle("s1");
+//        s1.initStyle(StageStyle.DECORATED);
+//        s1.show();
+//
+//        Stage s2 = new Stage();
+//        s2.setTitle("s2");
+//        s2.initStyle(StageStyle.TRANSPARENT);
+//        s2.show();
+
+//        Stage s3 = new Stage();
+//        s3.setTitle("s3");
+//        s3.initStyle(StageStyle.UNDECORATED);
+//        s3.show();
+
+//        Stage s4 = new Stage();
+//        s4.setTitle("s4");
+//        s4.initStyle(StageStyle.UNIFIED);
+//        s4.show();
+//
+//        Stage s5 = new Stage();
+//        s5.setTitle("s5");
+//        s5.initStyle(StageStyle.UTILITY);
+//        s5.show();
         /*how()方法之后设置默认的宽高，因此如果没有收动设置宽高，
         需要在show（)方法之后获取宽高，否则获取不到*/
-        primaryStage.show();
+        //primaryStage.show();
 
 
 
