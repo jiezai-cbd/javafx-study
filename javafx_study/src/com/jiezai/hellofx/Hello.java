106ec7982f9c95a3aab757929e17825000086726
jiezai-cbd <wwjpcx@163.com>
2020-06-09 16:32:08 +0800

    #

--- javafx_study/src/com/jiezai/hellofx/Hello.java
+++ javafx_study/src/com/jiezai/hellofx/Hello.java
@@ -2,18 +2,26 @@ package com.jiezai.hellofx;
 
 import com.sun.scenario.effect.impl.prism.PrImage;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.net.URL;
+
+/**
+ * @author jiezai_cbd
+ *
+ */
 public class Hello extends Application {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-
+//        System.out.println("HELLO WORLD");
         launch(args);
     }
 
@@ -21,6 +29,14 @@ public class Hello extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("JavaFX");
         primaryStage.getIcons().add(new Image("/icon/application.png"));
+
+        URL url = getClass().getClassLoader().getResource("icon/cursor_circle.png");
+        String path = url.toExternalForm();
+        Group group = new Group();
+        Scene scene = new Scene(group);
+        scene.setCursor(Cursor.cursor(path));
+        primaryStage.setScene(scene);
+        primaryStage.show();
         //primaryStage.setIconified(true);//设置最小化
         //primaryStage.setMaximized(true);//设置最大化
         //primaryStage.close();//关闭
@@ -90,9 +106,14 @@ public class Hello extends Application {
 //        s5.setTitle("s5");
 //        s5.initStyle(StageStyle.UTILITY);
 //        s5.show();
-        /*how()方法之后设置默认的宽高，因此如果没有收动设置宽高，
+//        Screen screen = Screen.getPrimary();
+//        double dpi = screen.getDpi();
+//        System.out.println("dpi:"+dpi);
+        /*show()方法之后设置默认的宽高，因此如果没有收动设置宽高，
         需要在show（)方法之后获取宽高，否则获取不到*/
-        //primaryStage.show();
+
+
+//        Platform.exit();
 
 
 
