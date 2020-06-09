a4cd438d059bdabf39cc20046fda3ecbd86b5e07
jiezai-cbd <wwjpcx@163.com>
2020-06-09 09:58:19 +0800

    添加图标

--- javafx_study/src/com/jiezai/hellofx/Hello.java
+++ javafx_study/src/com/jiezai/hellofx/Hello.java
@@ -1,6 +1,7 @@
 package com.jiezai.hellofx;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -13,6 +14,7 @@ public class Hello extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("JavaFX");
+        primaryStage.getIcons().add(new Image("/icon/application.png"));
         primaryStage.show();
     }
 }
